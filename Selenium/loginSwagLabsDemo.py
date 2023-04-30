43e7f8c9e3d08d423f9e35048098fb5820a4af1a
ahassid <33699232+ahassid@users.noreply.github.com>
2023-04-30 14:57:54 +0300

    Create loginSwagLabsDemo.py

---
+++ Selenium/loginSwagLabsDemo.py
@@ -0,0 +1,22 @@
+# selenium 4
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.chrome.service import Service as ChromiumService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
+
+driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+driver.get('https://www.saucedemo.com/')
+user = driver.find_element(By.ID, "user-name")
+user.click()
+user.clear()
+# search.send_keys("standard_user")
+user.send_keys("problem_user")
+password = driver.find_element(By.ID, "password")
+password.click()
+password.clear()
+password.send_keys("secret_sauce")
+login = driver.find_element(By.ID, "login-button")
+login.click()
+driver.close()
