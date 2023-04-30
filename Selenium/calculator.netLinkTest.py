0b5b3fe6a980dbf28df7dd89238bf38e84c7cba8
ahassid <33699232+ahassid@users.noreply.github.com>
2023-04-30 16:24:16 +0300

    Update calculator.netLinkTest.py

--- Selenium/calculator.netLinkTest.py
+++ Selenium/calculator.netLinkTest.py
@@ -9,12 +9,12 @@ from webdriver_manager.core.utils import ChromeType
 driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
 driver.get('https://www.calculator.net/')
 driver.maximize_window()
-salaryButton = driver.find_element(By.LINK_TEXT, "Salary Calculator")
-salaryButton_text = salaryButton.text
+salaryButtonLink = driver.find_element(By.LINK_TEXT, "Salary Calculator")
+salaryButton_text = salaryButtonLink.text
 if salaryButton_text == 'Salary Calculator':
     print('salary calculator text appears as expected')
-salaryButton.click()
-salaryButton = driver.find_element(By.PARTIAL_LINK_TEXT, "Fitness")
-salaryButton.click()
+salaryButtonLink.click()
+fitnessButtonLink = driver.find_element(By.PARTIAL_LINK_TEXT, "Fitness")
+fitnessButtonLink.click()
 
 driver.close()
