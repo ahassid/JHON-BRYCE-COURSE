587a5fd11aaabdcae7519b88ee6e46c58cde5af2
ahassid <33699232+ahassid@users.noreply.github.com>
2023-05-03 11:20:21 +0300

    Update WelcomePage.py

--- PageObject/Pages/WelcomePage.py
+++ PageObject/Pages/WelcomePage.py
@@ -11,6 +11,11 @@ class WelcomePage(object):
     def __init__(self, driver):
         self.driver = driver
 
-    def Welcome(self):
+    def getPrices(self):
         productsPrices = self.driver.find_elements(By.CLASS_NAME, "inventory_item_price")
         return productsPrices
+
+    def verifyTitle(self):
+        subTitle = self.driver.find_element(By.CSS_SELECTOR, "span[class='title']")
+        actualTitle = subTitle[0].text
+        assert actualTitle == 'Products', 'The title is not as expected'
